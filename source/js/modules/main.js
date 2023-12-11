4791c2ef9e5d07b7f67d3b644134794efe8de5bf
ialiev <alieff.ilya@gmail.com>
2023-12-11 13:41:22 +0700

    module1-task2: rm extra classes

--- source/js/modules/main.js
+++ source/js/modules/main.js
@@ -1,13 +1,7 @@
 export default () => {
   const body = document.querySelector(`.main`);
-  // const screenFooter = document.querySelector(`.js-footer`);
-  // const logo = document.querySelector(`.js-logo`);
-  // const nav = document.querySelector(`.js-nav`);
 
   window.addEventListener(`load`, () => {
     body.classList.add(`main--is-loaded`);
-    // nav.classList.add(`page-header__nav--is-visible`);
-    // logo.classList.add(`page-header__logo--is-visible`);
-    // screenFooter.classList.add(`screen__footer--is-visible`);
   });
 };
